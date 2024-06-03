a69bce7d13530824d338b9730c74baea152de68e
RikThePixel <rikdenbreejen@tutanota.com>
2024-06-03 10:43:29 +0200

    Removed obsolete tests

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '~/(.*)': '<rootDir>/src/$1',
-  },
-  testMatch: ['**/*.spec.(ts|tsx)'],
-  collectCoverage: false,
-};
